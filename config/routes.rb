1b71bff330532972c778cd027bec11f94ab61fcc
Monalisa <monalisadengre222@gmail.com>
2026-03-14 15:28:54 +0530

    Implement authentication system with signup, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +12,12 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "sessions#new"
+
+  get "signup", to: "registrations#new"
+  post "registrations", to: "registrations#create"
+
+  get "login", to: "sessions#new"
+  post "sessions", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
